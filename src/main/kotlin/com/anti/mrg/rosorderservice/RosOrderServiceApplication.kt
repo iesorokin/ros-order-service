ad2e408ca56f876a2802992a2b3d41f30f9dc930
Ivan <Ivan.Sorokin@leroymerlin.ru>
2018-11-10 21:22:56 +0300

    first init

---
+++ src/main/kotlin/com/anti/mrg/rosorderservice/RosOrderServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.anti.mrg.rosorderservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RosOrderServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RosOrderServiceApplication>(*args)
+}
